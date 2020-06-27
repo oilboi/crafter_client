2a7d0c35376a42cdafb9f61d7aec827b77955b18
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-27 18:22:24 -0400

    Bump up client mod version

--- version_send.lua
+++ version_send.lua
@@ -1,5 +1,5 @@
 local minetest,name = minetest,minetest.localplayer:get_name()
 local version_channel = minetest.mod_channel_join(name..":client_version_channel")
 minetest.after(2,function() -- this needs a few seconds for the mod channel to open up
-    version_channel:send_all("0.05011")
+    version_channel:send_all("0.06000")
 end)
